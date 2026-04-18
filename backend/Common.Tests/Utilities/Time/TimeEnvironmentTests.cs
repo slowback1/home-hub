8cf65c91db25ebcbd4e85cb63c6b83672b020624
andrew <dwobeck@gmail.com>
2026-04-18 08:11:36 -0500

    Update to .net 10.0

--- backend/Common.Tests/Utilities/Time/TimeEnvironmentTests.cs
+++ backend/Common.Tests/Utilities/Time/TimeEnvironmentTests.cs
@@ -6,31 +6,31 @@ namespace Common.Tests.Utilities.Time;
 
 public class TimeEnvironmentTests
 {
-    [TearDown]
-    public void Teardown()
-    {
-        TimeEnvironment.ResetProvider();
-    }
+	[TearDown]
+	public void Teardown()
+	{
+		TimeEnvironment.ResetProvider();
+	}
 
-    [Test]
-    public void HasATimeProvider()
-    {
-        var provider = TimeEnvironment.Provider;
-        Assert.NotNull(provider);
-    }
+	[Test]
+	public void HasATimeProvider()
+	{
+		var provider = TimeEnvironment.Provider;
+		Assert.That(provider, Is.Not.Null);
+	}
 
-    [Test]
-    public void DefaultTimeProviderIsTimeProvider()
-    {
-        var provider = TimeEnvironment.Provider;
-        Assert.IsInstanceOf<TimeProvider>(provider);
-    }
+	[Test]
+	public void DefaultTimeProviderIsTimeProvider()
+	{
+		var provider = TimeEnvironment.Provider;
+		Assert.That(provider, Is.InstanceOf<TimeProvider>());
+	}
 
-    [Test]
-    public void CanOverrideTheTimeProvider()
-    {
-        var provider = new TestTimeProvider(new DateTime(2022, 2, 2));
-        TimeEnvironment.SetProvider(provider);
-        Assert.That(TimeEnvironment.Provider, Is.SameAs(provider));
-    }
+	[Test]
+	public void CanOverrideTheTimeProvider()
+	{
+		var provider = new TestTimeProvider(new DateTime(2022, 2, 2));
+		TimeEnvironment.SetProvider(provider);
+		Assert.That(TimeEnvironment.Provider, Is.SameAs(provider));
+	}
 }
--- backend/Logic.Tests/GetExampleDataUseCaseTests.cs
+++ backend/Logic.Tests/GetExampleDataUseCaseTests.cs
@@ -5,17 +5,17 @@ namespace Logic.Tests;
 
 public class GetExampleDataUseCaseTests
 {
-    [Test]
-    public async Task Execute_ReturnsExpectedExampleData()
-    {
-        var useCase = new GetExampleDataUseCase(new InMemoryGenericCrud<ExampleData>());
+	[Test]
+	public async Task Execute_ReturnsExpectedExampleData()
+	{
+		var useCase = new GetExampleDataUseCase(new InMemoryGenericCrud<ExampleData>());
 
-        var result = await useCase.Execute();
+		var result = await useCase.Execute();
 
-        Assert.That(result.Status, Is.EqualTo(UseCaseStatus.Success));
-        Assert.NotNull(result.Result);
-        Assert.That(result.Result!.Id, Is.EqualTo("1"));
-        Assert.That(result.Result.Name, Is.EqualTo("Example"));
-        Assert.That(result.Result.Value, Is.EqualTo(42));
-    }
+		Assert.That(result.Status, Is.EqualTo(UseCaseStatus.Success));
+		Assert.That(result.Result, Is.Not.Null);
+		Assert.That(result.Result!.Id, Is.EqualTo("1"));
+		Assert.That(result.Result.Name, Is.EqualTo("Example"));
+		Assert.That(result.Result.Value, Is.EqualTo(42));
+	}
 }
